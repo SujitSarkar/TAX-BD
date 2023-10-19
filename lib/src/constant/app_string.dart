ac6e09cefc4e8548c16afed458afad673c4a7c17
Sujit Sarkar <snigdho532@gmail.com>
2023-10-19 12:49:18 +0600

    Configured for release mode

--- lib/src/constant/app_string.dart
+++ lib/src/constant/app_string.dart
@@ -1,14 +1,4 @@
 class AppString {
   static const String fontName = 'plusJakartaSans';
   static const String appName = 'Tax BD';
-
-  static const String loginSuccess = "Login Success";
-  static const String no = 'No';
-  static const String yes = 'Yes';
-  static const String noInternetConnection = 'No internet connection';
-  //StorageKey
-  static const String userIdKey = 'userId';
-  static const String userNameKey = 'userName';
-  static const String userEmailKey = 'userEmail';
-  static const String hasAttachmentKey = 'has_attachment';
 }
